f1d127553cd7389fc1ec75bd23842d26093c2016
denispyr <pyr.school@gmail.com>
2021-11-16 17:26:07 +0200

    Dummy CC validation

--- scripts/validators.js
+++ scripts/validators.js
@@ -1,3 +1,4 @@
-function validateCC() {
-    alert("CC validation not implemented yet")            
+function validateCC(number1, number2, number3, number4, ccv, holder, expYear, expMonth) {
+    console.log(`Received: ${number1}-${number2}-${number3}-${number4}`)
+    console.log(`Received: ${ccv}-${holder}-${expYear}-${expMonth}`)
 }
