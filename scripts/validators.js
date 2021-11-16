f1c40193ca50df3bf80854af2faeef928a0c3e36
denispyr <pyr.school@gmail.com>
2021-11-16 18:23:12 +0200

    Validate number1 (length, digits)

--- scripts/validators.js
+++ scripts/validators.js
@@ -1,4 +1,15 @@
 function validateCC(number1, number2, number3, number4, ccv, holder, expYear, expMonth) {
     console.log(`Received: ${number1}-${number2}-${number3}-${number4}`)
     console.log(`Received: ${ccv}-${holder}-${expYear}-${expMonth}`)
+
+    if (number1.length !== 4) {
+        return false
+    }
+
+    let isNum = /^\d+$/.test(number1)
+    if (!isNum) {
+        return false
+    }
+
+    return true
 }
