550b4c59e40eda3ea7f32f1a48f870494d6dffc5
denispyr <pyr.school@gmail.com>
2021-11-16 18:51:53 +0200

    Refactor: created cc number validator

--- scripts/validators.js
+++ scripts/validators.js
@@ -2,14 +2,36 @@ function validateCC(number1, number2, number3, number4, ccv, holder, expYear, ex
     console.log(`Received: ${number1}-${number2}-${number3}-${number4}`)
     console.log(`Received: ${ccv}-${holder}-${expYear}-${expMonth}`)
 
-    if (number1.length !== 4) {
+    if (validateCCNumber(number1)===false) {
         return false
     }
 
-    let isNum = /^\d+$/.test(number1)
-    if (!isNum) {
+    if (validateCCNumber(number2) === false) {
+        return false
+    }
+
+    if (validateCCNumber(number3) === false) {
+        return false
+    }
+
+    if (validateCCNumber(number4) === false) {
         return false
     }
 
     return true
+}
+
+
+function validateCCNumber(ccn) {
+    if (ccn.length !== 4) {
+        console.log(`[${ccn}] is not 4 characters long`)
+        return false
+    }
+
+    let isNum1 = /^\d+$/.test(ccn)
+    if (!isNum1) {
+        console.log(`[${ccn}] is not digits only`)
+        return false
+    }
+    return true;
 }
