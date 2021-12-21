2a144a297c77d020dbd654654c71a7563a8b0b5c
denispyr <pyr.school@gmail.com>
2021-12-21 19:04:23 +0200

    Validate using DOM (only the four cc numbers)

--- scripts/validators.js
+++ scripts/validators.js
@@ -1,7 +1,38 @@
 
+getCCDataFromDOM = () => {
+    const theCC = {
+        number1: '',
+        number2: '',
+        number3: '',
+        number4: '',
+        ccv: '',
+        holderName: '',
+        expYear: '',
+        expMonth: ''
+    }
+
+    theCC.number1 = document.getElementById("ccpart1").value
+    theCC.number2 = document.getElementById("ccpart2").value
+    theCC.number3 = document.getElementById("ccpart3").value
+    theCC.number4 = document.getElementById("ccpart4").value
+    theCC.ccv = document.getElementById("ccv").value
+    theCC.holderName = document.getElementById("holder").value
+    theCC.expYear = document.getElementById("exp_year").value
+    theCC.expMonth = document.getElementById("exp_month").value
+
+    console.log(theCC)
+
+    return theCC;
+}
+
+
 function validateCC(theCreditCard) {
 
     console.log(theCreditCard)
+
+    const errorDiv = document.getElementById("error")
+    errorDiv.style.visibility = "hidden"
+    errorDiv.innerHTML = ""
 
     if (!validateCCNumber(theCreditCard.number1) ||
         !validateCCNumber(theCreditCard.number2) ||
@@ -48,5 +79,11 @@ function passCCDataToTheBank(cc, amount, shopId) {
     alert("Passing credit card data to the bank")
 }
 
+
+handleCCError = (message) => {
+    const errorDiv = document.getElementById("error")
+    errorDiv.style.visibility = "visible"
+    errorDiv.innerHTML = "Cannot process credit card: " + message
+}
 
 
